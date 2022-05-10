34fbdfb64028534160d4c2dc06cabe3df27ac2b2
kf1434 <105286631+kf1434@users.noreply.github.com>
2022-05-10 22:27:58 +0900

    make notepad

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace notepad123
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 열기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                StreamReader r = new StreamReader(openFileDialog1.FileName);
+                richTextBox1.Text = r.ReadToEnd();
+                r.Close();
+            }
+
+        }
+
+        private void 저장ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(saveFileDialog1.ShowDialog()== DialogResult.OK)
+            {
+                StreamWriter w = new StreamWriter(saveFileDialog1.FileName);
+                w.WriteLine(richTextBox1.Text);
+                w.Close();
+            }
+        }
+
+        private void 종료ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
